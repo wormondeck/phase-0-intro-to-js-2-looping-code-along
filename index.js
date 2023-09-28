7ce87053cc8b86f80462128fa37e41331d6e5e39
Ronney Arias <ronneyarias@gmail.com>
2023-09-27 20:17:29 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+
+  function countDown(startNumber) {
+    for (let i = startNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(5);
